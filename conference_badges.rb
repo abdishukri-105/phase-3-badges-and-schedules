9ce3c6941efa7d2917b18d55a386f1bea8380a71
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-07 19:33:42 +0300

    passed all tests

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+require "pry"
+
+def badge_maker (name)
+     "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+   badge_messages = []
+   names.each do |name|
+    badge_messages << "Hello, my name is #{name}."
+   end
+   badge_messages
+end
+
+def assign_rooms(speakers)
+    room_assignments = []
+    speakers.each_with_index do |speaker, index|
+      room = index + 1
+      room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+    room_assignments
+  end
+  
+  def printer(speakers)
+    badge_messages = batch_badge_creator(speakers)
+    badge_messages.each { |message| puts message }
+  
+    room_assignments = assign_rooms(speakers)
+    room_assignments.each { |assignment| puts assignment }
+  end
+  
